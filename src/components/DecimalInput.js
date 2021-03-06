0bec1bf5f8eabe06122aadc100c7139427d43795
fhs44429 <lebner.mmt-b2019@fh-salzburg.ac.at>
2021-03-06 22:02:18 +0100

    Add MoneyTransactionCreate Form

--- src/components/DecimalInput.js
+++ src/components/DecimalInput.js
@@ -3,9 +3,9 @@ import Input from './Input';
 // import styles from './DecimalInput.module.css'
 
 const DecimalInput = ({
-  inputName, labelText, onClick, onChange, value,
+  inputName, id, labelText, onClick, onChange, value,
 }) => (
-  <Input type="number" labelText={labelText} name={inputName} onClick={onClick} onChange={(evt) => onChange(evt)} value={value}> </Input>
+  <Input type="number" id={id} labelText={labelText} name={inputName} onClick={onClick} onChange={(evt) => onChange(evt)} value={value}> </Input>
 );
 
 export default DecimalInput;
--- src/components/MoneyTransactionCreate.js
+++ src/components/MoneyTransactionCreate.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import { object, number } from 'yup';
+
 import Button from './Button';
 import DecimalInput from './DecimalInput';
 import SelectInput from './SelectInput';
+import Users from '../data/User.json';
 
-const MoneyTransactionCreate = () => (
-  <>
-    <form>
-      <SelectInput> </SelectInput>
-      <DecimalInput> </DecimalInput>
-      <Button> </Button>
-    </form>
-  </>
-);
+const validationSchema = object({
+  amount: number().positive(),
+});
+
+const MoneyTransactionCreate = () => {
+  const formik = useFormik({
+    initialValues: { userId: 0, amount: '0' },
+    validationSchema,
+    onSubmit: (values) => {
+      const creditorId = (values.userId === 1 ? 2 : 1);
+      console.log(`Trying to create amount: ${values.amount}, from User: ${creditorId} to User: ${values.userId}`);
+    },
+  });
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <SelectInput
+          options={Users}
+          inputName="userId"
+          labelText="User"
+          onChange={formik.handleChange}
+          id="userId"
+          value={formik.values.userId}
+        />
+        <DecimalInput
+          inputName="amount"
+          labelText="Amount"
+          onChange={formik.handleChange}
+          value={formik.values.amount}
+          id="amount"
+        />
+        <Button
+          onClick={formik.handleSubmit}
+          disabled={!(formik.isValid && formik.dirty)}
+        >
+          {' '}
+          Create
+        </Button>
+      </form>
+    </>
+  );
+};
 
 export default MoneyTransactionCreate;
